235323880c7203e2ba88bd434cf51aa7095d683b
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-04-16 12:08:04 -0600

    make main a 'module'

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
-/** */
+/**
+ * @file Main entrypoint
+ * @copyright 2023 Matthew A. Miller
+ */
 
 import { resolve } from "../deps/src/path.ts";
 import { Server } from "./server.ts";
 import logger from "./util/log.ts";
 
-// TODO: module-ize
-const root = resolve(Deno.args[0] || ".");
+if (import.meta.main) {
+  const root = resolve(Deno.args[0] || ".");
 
-const abort = new AbortController();
-const { signal } = abort;
-Deno.addSignalListener("SIGINT", () => {
-  logger.warning("stopping");
-  abort.abort();
-});
+  const abort = new AbortController();
+  const { signal } = abort;
+  Deno.addSignalListener("SIGINT", () => {
+    logger.warning("stopping");
+    abort.abort();
+  });
 
-const server = new Server(root);
-await server.serve({
-  signal,
-});
-logger.warning("stopped");
+  const server = new Server(root);
+  await server.serve({
+    signal,
+  });
+  logger.warning("stopped");
+}
