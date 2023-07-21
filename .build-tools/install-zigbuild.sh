7cd822ff5f2df7ca27e3efb787e013a0c8faf879
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-07-20 21:42:52 -0600

    Revert "don't force zigbuild"
    This reverts commit ba64d0f25c3c4c4afb7f9ff592e630e45f6655ef.

--- .build-tools/install-zigbuild.sh
+++ .build-tools/install-zigbuild.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 ZIGBUILD_VERSION=0.16.12
 
 if [[ $(cargo install --list | grep "cargo-zigbuild v${ZIGBUILD_VERSION}") != "" ]] ; then
   echo "cargo-zigbuild v${ZIGBUILD_VERSION} already installed"
 else
-  cargo install --version ${ZIGBUILD_VERSION} cargo-zigbuild
+  cargo install --version 0.16.12 cargo-zigbuild --force
   echo "cargo-zigbuild v${ZIGBUILD_VERSION} installed"
 fi
