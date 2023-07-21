77fd154613bab0c05ba212b15ff354e3496a5a22
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-07-20 21:37:57 -0600

    nit: print zibuild installed

--- .build-tools/install-zigbuild.sh
+++ .build-tools/install-zigbuild.sh
@@ -8,4 +8,5 @@ if [[ $(cargo install --list | grep "cargo-zigbuild v${ZIGBUILD_VERSION}") != ""
   echo "cargo-zigbuild v${ZIGBUILD_VERSION} already installed"
 else
   cargo install --version 0.16.12 cargo-zigbuild --force
+  echo "cargo-zigbuild v${ZIGBUILD_VERSION} installed"
 fi
