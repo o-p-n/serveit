c1e9ea8e866bb9cf4114077eab67104eb5c91d31
Matthew A. Miller <linuxwolf@outer-planes.net>
2025-06-27 09:02:28 -0600

    move config to common types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,12 @@
-import { Config } from "./config.ts";
+import { type LogLevel } from "@logtape/logtape";
+
+export interface Config {
+  rootDir: string;
+  port: number;
+  metaPort: number;
+  logLevel: LogLevel | null;
+}
+
 
 export interface ServerConfig extends Config {
   signal?: AbortSignal;
