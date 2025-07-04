aab36a9c2f318d2cc249c33b47ce1129c3bf5961
Matthew A. Miller <linuxwolf@outer-planes.net>
2025-07-04 10:01:39 -0600

    basic cache implementation

---
+++ src/files/cache.ts
@@ -0,0 +1,88 @@
+/**
+ * @copyright 2025 Matthew A. Miller
+ */
+
+import { encodeHex } from "@std/encoding";
+import { walk } from "@std/fs";
+import { typeByExtension } from "@std/media-types";
+import { basename, dirname, extname, join, resolve } from "@std/path";
+
+import { FileEntry } from "./entry.ts";
+
+export const _internals = {
+  readFile: Deno.readFile,
+  stat: Deno.stat,
+  walk,
+};
+
+
+export class FileCache {
+  readonly rootDir: string;
+  private contents: Record<string, FileEntry> = {};
+
+  constructor(rootDir: string, prefill?: Record<string, FileEntry>) {
+    this.rootDir = rootDir;
+    this.contents = { ...prefill };
+  }
+
+  get files(): Record<string, FileEntry> {
+    return { ...this.contents };
+  }
+
+  async index() {
+    const itr = _internals.walk(this.rootDir, {
+      includeDirs: false,
+      includeSymlinks: false,
+    });
+
+    const cache: Record<string, FileEntry> = {};
+    for await (const fe of itr) {
+      const path = fe.path;
+      const key = path.substring(this.rootDir.length);
+      const stat = await _internals.stat(path);
+      const content = await _internals.readFile(path);
+      const etag = await calculateETag(content);
+
+      const type = typeByExtension(extname(path)) || "text/plain";
+      const size = stat.size;
+      const createdAt = stat.birthtime || new Date();
+      const modifiedAt = stat.mtime || createdAt;
+
+      const entry = new FileEntry({
+        path,
+        type,
+        size,
+        createdAt,
+        modifiedAt,
+        etag,
+      });
+      cache[key] = entry;
+      
+      if (/index\.[^.]*$/g.test(basename(path)) && (type === "text/html")) {
+        // record directory entry for index file
+        const index = resolve(dirname(key));
+        cache[index] = entry;
+      }
+    }
+
+    // replace existing contents
+    this.contents = cache;
+  }
+
+  async find(pathname: string): Promise<FileEntry> {
+    let entry = this.contents[pathname];
+    if (!entry) {
+      const path = join(this.rootDir, pathname);
+      // content etag not calcuated — not adding to cache 
+      entry = await FileEntry.find(path);
+    }
+    return entry;
+  }
+}
+
+async function calculateETag(content: Uint8Array) {
+  const digest = await crypto.subtle.digest("SHA-256", content);
+  const hash = encodeHex(digest);
+
+  return hash;
+}
---
+++ test/files/cache.test.ts
@@ -0,0 +1,319 @@
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  it,
+} from "../deps.ts";
+import { expect, FakeTime, mock } from "../deps.ts";
+
+import { FileCache, _internals } from "../../src/files/cache.ts";
+import { WalkEntry, WalkOptions } from "@std/fs";
+import { join } from "@std/path";
+import { toStat } from "./common.ts";
+import { FileEntry } from "../../src/files/entry.ts";
+
+describe("files/cache", () => {
+  describe("FileCache", () => {
+    describe(".ctor()", () => {
+      it("creates with empty contents", () => {
+        const cache = new FileCache("/app/web");
+        expect(cache.rootDir).to.equal("/app/web");
+        expect(cache.files).to.deep.equal({});
+      });
+      it("creates with prefilled contents", () => {
+        const contents: Record<string, FileEntry> = {
+          "file.txt": new FileEntry({
+            path: "/app/web/file.txt",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "abcdefg",
+          }),
+          "image.png": new FileEntry({
+            path: "/app/web/image.png",
+            type: "image/png",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "abcdefg",
+          }),
+        }
+        const cache = new FileCache("/app/web", contents);
+        expect(cache.rootDir).to.equal("/app/web");
+        expect(cache.files).to.deep.equal(contents);
+
+        delete contents["image.png"];
+        expect(cache.files).to.not.deep.equal(contents);
+      });
+    });
+
+    describe(".index()", () => {
+      const seedTime = new Date(10000);
+      let clock: FakeTime;
+
+      let cache: FileCache;
+      let spyWalk: mock.Spy;
+      let spyStat: mock.Spy;
+      let spyReadFile: mock.Spy;
+
+      beforeEach(() => {
+        clock = new FakeTime(seedTime);
+
+        cache = new FileCache("/app/web");
+        spyWalk = mock.stub(
+          _internals,
+          "walk",
+          async function* (root: string | URL, _opts?: WalkOptions) {
+            const result: WalkEntry[] = [
+              {
+                path: join(root.toString(), "file1.txt"),
+                name: "file1.txt",
+                isDirectory: false,
+                isFile: true,
+                isSymlink: false,
+              },
+              {
+                path: join(root.toString(), "file2.txt"),
+                name: "file2.txt",
+                isDirectory: false,
+                isFile: true,
+                isSymlink: false,
+              },
+              {
+                path: join(root.toString(), "output.random"),
+                name: "output.random",
+                isDirectory: false,
+                isFile: true,
+                isSymlink: false,
+              },
+              {
+                path: join(root.toString(), "subdir", "index.html"),
+                name: "index.html",
+                isDirectory: false,
+                isFile: true,
+                isSymlink: false,
+              },
+              {
+                path: join(root.toString(), "otherdir", "index.htm"),
+                name: "index.html",
+                isDirectory: false,
+                isFile: true,
+                isSymlink: false,
+              },
+            ];
+
+            for (const entry of result) {
+              yield entry;
+            }
+          },
+        );
+        spyStat = mock.stub(_internals, "stat", (path: string | URL) => {
+          const birthtime = (path.toString().endsWith(".html")) ? null : new Date(30000);
+          const mtime = (path.toString().endsWith("file2.txt")) ? null : new Date(120000);
+          return Promise.resolve(toStat({
+            isFile: true,
+            birthtime,
+            mtime,
+          }));
+        });
+        spyReadFile = mock.stub(_internals, "readFile", (_path: string | URL, _opts?: Deno.ReadFileOptions) => {
+          // "file content"
+          return Promise.resolve(new Uint8Array([  102, 105, 108, 101, 32,  99, 111, 110, 116, 101, 110, 116]));
+        });
+      });
+
+      afterEach(() => {
+        spyWalk.restore();
+        spyStat.restore();
+        spyReadFile.restore();
+        clock.restore();
+      });
+
+      it("indexes the rootDir", async () => {
+        await cache.index();
+        expect(cache.files).to.deep.equal({
+          "/file1.txt": new FileEntry({
+            path: "/app/web/file1.txt",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(30000),
+            modifiedAt: new Date(120000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+          "/file2.txt": new FileEntry({
+            path: "/app/web/file2.txt",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(30000),
+            modifiedAt: new Date(30000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+          "/output.random": new FileEntry({
+            path: "/app/web/output.random",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(30000),
+            modifiedAt: new Date(120000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+          "/subdir": new FileEntry({
+            path: "/app/web/subdir/index.html",
+            type: "text/html",
+            size: 1000,
+            createdAt: new Date(10000),
+            modifiedAt: new Date(120000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+          "/subdir/index.html": new FileEntry({
+            path: "/app/web/subdir/index.html",
+            type: "text/html",
+            size: 1000,
+            createdAt: new Date(10000),
+            modifiedAt: new Date(120000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+          "/otherdir": new FileEntry({
+            path: "/app/web/otherdir/index.htm",
+            type: "text/html",
+            size: 1000,
+            createdAt: new Date(30000),
+            modifiedAt: new Date(120000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+          "/otherdir/index.htm": new FileEntry({
+            path: "/app/web/otherdir/index.htm",
+            type: "text/html",
+            size: 1000,
+            createdAt: new Date(30000),
+            modifiedAt: new Date(120000),
+            etag: "e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c",
+          }),
+        });
+
+        expect(spyWalk).to.have.been.deep.calledWith([
+          "/app/web",
+          {
+            includeDirs: false,
+            includeSymlinks: false,
+          },
+        ]);
+
+        expect(spyStat).to.have.been.called(5);
+        expect(spyStat).to.have.been.deep.calledWith([
+          "/app/web/file1.txt",
+        ]);
+        expect(spyStat).to.have.been.deep.calledWith([
+          "/app/web/file2.txt",
+        ]);
+        expect(spyStat).to.have.been.deep.calledWith([
+          "/app/web/output.random",
+        ]);
+        expect(spyStat).to.have.been.deep.calledWith([
+          "/app/web/subdir/index.html",
+        ]);
+        expect(spyStat).to.have.been.deep.calledWith([
+          "/app/web/otherdir/index.htm",
+        ]);
+
+        expect(spyReadFile).to.have.been.called(5);
+        expect(spyReadFile).to.have.been.deep.calledWith([
+          "/app/web/file1.txt",
+        ]);
+        expect(spyReadFile).to.have.been.deep.calledWith([
+          "/app/web/file2.txt",
+        ]);
+        expect(spyReadFile).to.have.been.deep.calledWith([
+          "/app/web/output.random",
+        ]);
+        expect(spyReadFile).to.have.been.deep.calledWith([
+          "/app/web/subdir/index.html",
+        ]);
+        expect(spyReadFile).to.have.been.deep.calledWith([
+          "/app/web/otherdir/index.htm",
+        ]);
+      });
+    });
+
+    describe(".find()", () => {
+      let cache: FileCache;
+      let spyEntryFind: mock.Spy;
+
+      beforeEach(() => {
+        cache = new FileCache("/app/web", {
+          "/file1.txt": new FileEntry({
+            path: "/app/web/file1.txt",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "0123456789abcdef",
+          }),
+          "/subdir": new FileEntry({
+            path: "/app/web/subdir/index.html",
+            type: "text/html",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "0123456789abcdef",
+          }),
+          "/subdir/index.html": new FileEntry({
+            path: "/app/web/subdir/index.html",
+            type: "text/html",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "0123456789abcdef",
+          }),
+        });
+        spyEntryFind = mock.stub(FileEntry, "find", (path: string) => {
+          return Promise.resolve(new FileEntry({
+            path,
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "0123456789abcdef",
+          }));
+        });
+      });
+
+      afterEach(() => {
+        spyEntryFind.restore();
+      });
+
+      it("file path returns the cached entry", async () => {
+        const result = await cache.find("/file1.txt");
+
+        expect(result).to.deep.equal(new FileEntry({
+            path: "/app/web/file1.txt",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "0123456789abcdef",
+        }));
+        expect(result).to.equal(cache.files["/file1.txt"]);
+
+        expect(spyEntryFind).to.not.have.been.called();
+      });
+      it("file path falls through entry find", async () => {
+        const result = await cache.find("/file2.txt");
+
+        expect(result).to.deep.equal(new FileEntry({
+            path: "/app/web/file2.txt",
+            type: "text/plain",
+            size: 1000,
+            createdAt: new Date(60000),
+            modifiedAt: new Date(120000),
+            etag: "0123456789abcdef",
+        }));
+        expect(cache.files["/file2.txt"]).to.not.exist();
+
+        expect(spyEntryFind).to.have.been.deep.calledWith([
+          "/app/web/file2.txt",
+        ]);
+      });
+    });
+  });
+});
